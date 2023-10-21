d9654348b47b5111b8eec22b61b75f75af1d2f17
dimak2107 <dimak2107@gmail.com>
2023-10-21 13:37:01 +0500

    added mui buttons for todolist

--- src/components/toDoItem/ToDoItem.tsx
+++ src/components/toDoItem/ToDoItem.tsx
@@ -1,36 +1,50 @@
-import React from "react";
 import { BiCheckCircle } from "react-icons/bi";
 import { ImCross } from "react-icons/im";
 import { AiFillDelete } from "react-icons/ai";
+import Button from "@mui/material/Button";
 import "./ToDoItem.css";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import DoneIcon from "@mui/icons-material/Done";
+import CloseIcon from "@mui/icons-material/Close";
+import { red } from "@mui/material/colors";
 
-interface item {
-  id: number;
-  title: string;
-  isCompleted: boolean;
+interface Item {
+  item: {
+    id: number;
+    title: string;
+    isCompleted: boolean;
+  };
+  changeTodo?(id: number): void;
+  removeTodo?(id: number): void;
 }
 
-const ToDoItem = ({ item, changeTodo, removeTodo }: item) => {
+const ToDoItem = ({ item, changeTodo, removeTodo }: Item) => {
   return (
     <div className="app__todoitem">
       <div className="app__todoitem-content">
         {item.isCompleted ? (
-          <BiCheckCircle
+          <IconButton
             className="app__todoitem-content_btns"
+            size="small"
             onClick={() => changeTodo(item.id)}
-          />
+          >
+            <DoneIcon color="success" />
+          </IconButton>
         ) : (
-          <ImCross
-            className="app__todoitem-content_btns"
-            onClick={() => changeTodo(item.id)}
-          />
+          <IconButton size="small" onClick={() => changeTodo(item.id)}>
+            <CloseIcon sx={{ color: red[500] }} />
+          </IconButton>
         )}
         <div className="app__todoitem-text">{item.title}</div>
         <div>
-          <AiFillDelete
-            className="app__todoitem-content_btns-delete"
+          <IconButton
+            size="large"
+            aria-label="delete"
             onClick={() => removeTodo(item.id)}
-          />
+          >
+            <DeleteIcon sx={{ color: red[900] }} />
+          </IconButton>
         </div>
       </div>
     </div>
